a46426aeaa2ad4479372b1a95132ce9be3509306
srinath <sriv5@host.docker.internal>
2020-10-21 00:11:20 -0400

    first commit

---
+++ FirstProject/src/test/java/firstpackage/FirstclassTest.java
@@ -0,0 +1,15 @@
+package firstpackage;
+
+import org.testng.annotations.Test;
+
+public class FirstclassTest {
+	
+	@Test
+	public void firstclasstest() {
+		
+		System.out.println("*************");
+		System.out.println("    Done     ");
+		System.out.println("*************");
+	}
+
+}
